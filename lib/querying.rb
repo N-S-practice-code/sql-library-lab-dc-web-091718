35882e426a1746be7fe7b890bde5d8edb9c0c0d7
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-01 14:19:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books NNER JOIN (series LIMIT 1)  ORDER BY gpa DESC LIMIT 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT title, year FROM students ORDER BY gpa DESC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT value, count FROM characters ORDER BY gpa DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT name, tital FROM students ORDER BY gpa DESC LIMIT 1"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title, year FROM seriesjoin characters ORDER BY gpa DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT name, count FROM characters ORDER BY gpa DESC LIMIT 1"
 end
